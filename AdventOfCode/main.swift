a9b4c01d9dd061fa07cb877337dcdc8f132cde83
Anton Roslund <enari@minit.nu>
2018-12-02 13:11:51 +0100

    Initial Commit

---
+++ AdventOfCode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventOfCode
+//
+//  Created by Anton Roslund on 2018-12-02.
+//  Copyright © 2018 Anton Roslund. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
