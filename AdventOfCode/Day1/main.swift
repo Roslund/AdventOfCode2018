3c754ee3a344af27c92c30b9d4f1720782f7c9bc
Anton Roslund <enari@minit.nu>
2018-12-02 13:31:24 +0100

    Added solutions for Day1

---
+++ AdventOfCode/Day1/main.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+// Transform the puzzle input to an array of ints
+let frequencyChanges = input
+    .components(separatedBy: .newlines)
+    .compactMap { Int($0) }
+
+// MARK: - Puzzle 1
+print(frequencyChanges.reduce(0, +))
+
+// MARK: - Puzzle 2
+var seenFrequencies: [Int] = []
+var current = 0
+
+while true {
+    for input in frequencyChanges {
+        current += input;
+
+        if seenFrequencies.contains(current) {
+            print(current)
+            exit(0)
+        }
+        seenFrequencies.append(current)
+    }
+}
