80bdd4b9749510f65f206897b304400650dcb7df
Anton Roslund <enari@minit.nu>
2018-12-07 21:30:31 +0100

    Cleaned up the code for day 6

--- AdventOfCode/Day6/main.swift
+++ AdventOfCode/Day6/main.swift
@@ -16,50 +16,45 @@ let bounds: (x: (min: Int, max: Int), y: (min: Int, max: Int)) =
 
 
 // Mark: - Puzzle 1
-// If a point is closes to the bounds, the points closest
-// to it is infinit, since the problem is ounbound.
-// First we collect the coordinates that contain infinit points
+// If a coordinate is closest to at least one point on the edge of the
+// coordinate space: That coordinate will be closest to an infinit number
+// of points. Therefore we calculate all the points that are on the edge.
 let edgePoints = (bounds.x.min ... bounds.x.max).flatMap { x in
-    [Point(x, bounds.y.min), Point(x, bounds.y.max)]
+        [Point(x, bounds.y.min), Point(x, bounds.y.max)]
     } + (bounds.y.min ... bounds.y.max).flatMap { y in
         [Point(bounds.x.min, y), Point(bounds.x.max, y)]
 }
 
-// Create a dictionary keyed by pointes
-// containing the number of points that
-// is losest to each point.
+// Dictionary keyed by pointes. Suposed to contain the number of points that
+// is cloosest to each coordinate.
 var pointCSA = Dictionary(uniqueKeysWithValues: coordinates.map({($0, 0)}))
 
-// Populate the dictionary
+// Below we populate the pointCSA dictionary.
 for x in bounds.x.min ... bounds.x.max {
     for y in bounds.y.min ... bounds.y.max {
-        var closest: [Point] = []
-        var closestDistance = Int.max
+        // Order all coordinated by distance to the point at (x,y)
+        var ordered: [(coordinate: Point, distance: Int)] = coordinates
+            .map { ($0, $0.distance(to: Point(x, y))) }
+            .sorted { $0.1 < $1.1 }
 
-        for coordinate in coordinates {
-            let distance = Point(x, y).distance(to: coordinate)
-            if distance < closestDistance {
-                closestDistance = distance
-                closest = [coordinate]
-            } else if distance == closestDistance {
-                closest.append(coordinate)
-            }
-        }
+        // If there are more than two point at equal distance
+        // neither should count as beeing the cloosest.
+        if ordered[0].distance == ordered[1].distance { continue }
 
-        if closest.count == 1 {
-            // First we check if we are an edge point
-            // If so we remove it from teh dictionary
-            if edgePoints.contains(Point(x,y)){
-                pointCSA.removeValue(forKey: closest.first!)
-            } else {
-                pointCSA[closest.first!]? += 1
-            }
+        let closest = ordered.first!.coordinate
+
+        // Increase the count of points for the closest coordinate
+        pointCSA[closest]? += 1
+
+        // If we are at an edge we remove the point from the dictionary
+        if edgePoints.contains(Point(x,y)){
+            pointCSA.removeValue(forKey: closest)
         }
     }
 }
 
 // The we take the max value from the dictionary
-print(pointCSA.max(by: { a, b in a.value < b.value })!)
+print("Puzzle 1:", pointCSA.values.max()!)
 
 // Mark: - Puzzle 2
 var pointsWithinRegion = 0
@@ -75,4 +70,4 @@ for x in bounds.x.min ... bounds.x.max {
     }
 }
 
-print(pointsWithinRegion)
+print("Puzzle 2:", pointsWithinRegion)
