b04d3974b12a78ae197f7e09b13d85d232f7aea8
Anton Roslund <enari@minit.nu>
2018-12-10 17:43:02 +0100

    Small updates to day 10

--- AdventOfCode/Day10/main.swift
+++ AdventOfCode/Day10/main.swift
@@ -8,21 +8,26 @@
 
 import Foundation
 
+// Bara leta efter när skillnaden i y-led är minst
 var state = particles
-var min = Int.max
+var min = (0, Int.max)
 
 for _ in 0 ..< 10415 {
     state = state.step()
 }
 
 for i in 10415 ... 10513 {
-    state = state.step()
-    let area = state.area()
-    print(i, ":", area, ":", state.positions().bounds())
+    let bounds = state.positions().bounds()
+    let yDiff = bounds.y.max - bounds.y.min
 
-    if area < min {
-        min = area
+    print(i, ":", yDiff, ":", state.positions().bounds())
+
+    if yDiff <= min.1 {
+        min = (i, yDiff)
     }
+    state = state.step()
 }
+
+// This is the state we should draw...
 print(min)
 
