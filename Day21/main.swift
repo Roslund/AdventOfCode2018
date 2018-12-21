9f1f844a6e17020c1990132679ce24bd7257b30a
Anton Roslund <enari@minit.nu>
2018-12-21 23:14:17 +0100

    Added day 21

---
+++ Day21/main.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+//Mark: - Puzzle 1
+var program = Program(instructions, registers: [0,0,0,0,0,0], pcRegister: pcRegister)
+program.execute()
+
+//Mark: - Puzzle 2
+//Just bruteforce it... took 8 min.
